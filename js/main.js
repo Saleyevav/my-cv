de249d1584791f5f7e39516c0cd7ac07f0b2e73e
Alexandr Saleyev <saleyevav@yandex.ru>
2022-06-01 14:55:42 +0300

    Fix scroll nav bar

--- js/main.js
+++ js/main.js
@@ -23,7 +23,6 @@ const throttle = (func, limit) => {
 
 let y = 0;
 document.addEventListener('scroll', throttle(function () {
-    console.log(window.pageYOffset)
     element = document.getElementById("navBar");
     let delta = window.pageYOffset - y;
     if (window.pageYOffset > 700) {
@@ -34,25 +33,38 @@ document.addEventListener('scroll', throttle(function () {
             element.classList.add("nav-scroll-active");
         }
     } else if (window.pageYOffset < 700) {
-         element.classList.remove("nav-scroll-active");
+        element.classList.remove("nav-scroll-active");
     }
     y = window.pageYOffset;
 }, 100));
 
 
 let buttons = document.querySelectorAll('a[href^="#"]');
-
 for (let btn of buttons) {
     btn.onclick = function (e) {
         e.preventDefault();
         href = btn.getAttribute('href');
         let anchor = document.querySelector('a[name$="' + href.slice(1) + '"]');
-        anchor.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        });
-
+        if (href == "#home") {
+            window.scrollTo({
+                behavior: 'smooth',
+                top: 0
+            });
+        } else {
+            anchor.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
     }
+}
+
+let download = document.getElementById('downloadCVbtn');
+download.onclick = function (e) {
+    window.scrollTo({
+        behavior: 'smooth',
+        top: 100
+    });
 }
 
 function typeWriter(id, speed, txt, i) {
